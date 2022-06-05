59582ab2c5791f9dc0609c66e205134421fe538b
antonchik <ganseanderson@gmail.com>
2022-06-05 15:28:46 +0300

    Урок1.Создание каталога товаров,ввод через форму и отражение на листе.Применение принципов MVC, ознакомление с инструментарием Razor.

---
+++ WebApp_MVC/Controllers/GoodsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_MVC.Models;
+
+namespace WebApp_MVC.Controllers
+{
+    public class GoodsController : Controller
+    {
+        private Catalog _catalog;
+        public GoodsController(Catalog catalog)
+        {
+            _catalog = catalog;
+        }
+
+        [HttpPost]
+        public IActionResult Goods([FromForm] Good model)
+        {
+
+            _catalog.Goods.Add(model);
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Goods()
+        {
+            return View(_catalog);
+        }
+    }
+}
---
+++ WebApp_MVC/Models/Catalog.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApp_MVC.Models
+{
+    public class Catalog
+    {
+        public List<Good> Goods { get; set; }= new List<Good>();
+    }
+}
